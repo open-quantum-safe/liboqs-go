cd69cde078908ee423f6874fc86af612f9f74d05
Vlad Gheorghiu <vsoftco@gmail.com>
2019-10-21 13:26:25 -0400

    commit

--- oqstests/kem_test.go
+++ oqstests/kem_test.go
@@ -1,5 +1,5 @@
-// Package oqstest provides unit testing for the oqs Go package
-package tests // import "github.com/open-quantum-safe/liboqs-go/tests"
+// Package oqstests provides unit testing for the oqs Go package
+package oqstests // import "github.com/open-quantum-safe/liboqs-go/oqstests"
 
 import (
     "bytes"
--- oqstests/sig_test.go
+++ oqstests/sig_test.go
@@ -1,4 +1,4 @@
-package tests
+package oqstests
 
 import (
     "fmt"
