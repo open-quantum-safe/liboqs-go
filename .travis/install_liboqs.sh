379f7101e79d769982e33a9d8dbae2805c7dc55e
xvzcf <xvzcf@users.noreply.github.com>
2020-11-16 17:00:17 -0500

    liboqs/master -> liboqs/main and refactoring.

--- .travis/install_liboqs.sh
+++ .travis/install_liboqs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-git clone https://github.com/open-quantum-safe/liboqs
+git clone --branch main --single-branch --depth 1 https://github.com/open-quantum-safe/liboqs.git
 cd liboqs
 git checkout master
 mkdir build && cd build
-cmake -GNinja -DBUILD_SHARED_LIBS=ON ..
+cmake -GNinja -DBUILD_SHARED_LIBS=ON -DOQS_BUILD_ONLY_LIB=ON ..
 ninja
 sudo ninja install
