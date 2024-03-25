160c0a967538fbba10e6c0ecb283566da39c1d2f
Alexander Scheel <alexander.m.scheel@gmail.com>
2024-03-24 21:21:34 -0400

    Remove NIST KAT random APIs
    In cc453db4a6e02d97bf5fc974840ace881ffa6da1 of liboqs, NIST KAT random
    helpers were removed from the public API interface of liboqs, causing
    liboqs-go to fail to build. Remove these functions as there's no way to
    support them anymore (and likely don't have much value).
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- examples/rand/rand.go
+++ examples/rand/rand.go
@@ -3,9 +3,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/open-quantum-safe/liboqs-go/oqs"
 	"log"
 	"runtime"
+
+	"github.com/open-quantum-safe/liboqs-go/oqs"
 
 	oqsrand "github.com/open-quantum-safe/liboqs-go/oqs/rand" // RNG support
 )
@@ -21,19 +22,6 @@ func CustomRNG(randomArray []byte, bytesToRead int) {
 
 func main() {
 	fmt.Println("liboqs version: " + oqs.LiboqsVersion())
-
-	if err := oqsrand.RandomBytesSwitchAlgorithm("NIST-KAT"); err != nil {
-		log.Fatal(err)
-	}
-	// set the entropy seed to some values
-	var entropySeed [48]byte
-	for i := 0; i < 48; i++ {
-		entropySeed[i] = byte(i)
-	}
-	if err := oqsrand.RandomBytesNistKatInit256bit(entropySeed, nil); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%-18s% X\n", "NIST-KAT: ", oqsrand.RandomBytes(32))
 
 	if err := oqsrand.RandomBytesCustomAlgorithm(CustomRNG); err != nil {
 		log.Fatal(err)
